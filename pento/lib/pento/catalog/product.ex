9cb2dd0ad671a8cc7ac21ee18936e5f84d643efb
AndrewG <andrewalangm@gmail.com>
2021-06-22 20:25:42 -0500

    🕵️‍♂️ Add some validations and seeds

--- pento/lib/pento/catalog/product.ex
+++ pento/lib/pento/catalog/product.ex
@@ -11,11 +11,17 @@ defmodule Pento.Catalog.Product do
     timestamps()
   end
 
+  def validate_price(product) do
+    product
+    |> validate_number(:unit_price, greater_than: 0.0)
+  end
+
   @doc false
   def changeset(product, attrs) do
     product
     |> cast(attrs, [:name, :description, :unit_price, :sku])
     |> validate_required([:name, :description, :unit_price, :sku])
     |> unique_constraint(:sku)
+    |> validate_price()
   end
 end
