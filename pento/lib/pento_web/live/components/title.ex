975e5cbff5dd8bc9d1e10656358af6a1cdde446b
AndrewG <andrewalangm@gmail.com>
2021-06-30 14:52:29 -0500

    🎟 Use preload function for components

--- pento/lib/pento_web/live/components/title.ex
+++ pento/lib/pento_web/live/components/title.ex
@@ -1,9 +1,16 @@
 defmodule PentoWeb.Live.Components.Title do
   use PentoWeb, :live_component
 
+  def preload(list_of_assigns) do
+    Enum.map(list_of_assigns, fn %{id: id, title: title} ->
+      %{id: id, title: "#{title} #{id}", subtitle: "Subtitle #{id}"}
+    end)
+  end
+
   def render(assigns) do
     ~L"""
     <h1> <%= @title %> </h1>
+    <h2> <%= @subtitle %> </h2>
     """
   end
 end
