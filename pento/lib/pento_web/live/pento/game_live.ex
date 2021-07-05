bc5d3ec110a224efaa4567bfb41de8099777f2fe
AndrewG <andrewalangm@gmail.com>
2021-07-05 16:24:26 -0500

    Create canvas component

--- pento/lib/pento_web/live/pento/game_live.ex
+++ pento/lib/pento_web/live/pento/game_live.ex
@@ -1,13 +1,19 @@
 defmodule PentoWeb.Pento.GameLive do
   use Surface.LiveView
-  alias PentoWeb.Pento.Title
+  alias PentoWeb.Pento.{Canvas, Point, Title}
 
   def mount(_params, _session, socket), do: {:ok, socket}
 
   def render(assigns) do
     ~F"""
     <section class="container">
-      <Title message={4}/>
+      <Title message="Welcome to Pento!" />
+      <Canvas viewBox="0 0 200 30">
+        <Point x={0} y={0} fill="blue" name="a" />
+        <Point x={1} y={0} fill="green" name="b" />
+        <Point x={0} y={1} fill="red" name="c" />
+        <Point x={1} y={1} fill="black" name="d" />
+      </Canvas>
     </section>
     """
   end
